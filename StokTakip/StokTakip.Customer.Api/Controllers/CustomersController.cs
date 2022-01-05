35bbcc54d0f58f8eae3bc860d4abdf2fca9adf78
Ahmet CANDAN <candanahm@gmail.com>
2022-01-05 22:46:43 +0300

    test

--- StokTakip/StokTakip.Customer.Api/Controllers/CustomersController.cs
+++ StokTakip/StokTakip.Customer.Api/Controllers/CustomersController.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 
 namespace StokTakip.Customer.Api.Controllers
 {
+    //Test commit
     [Authorize(Roles = "customer")]
     [ApiController]
     [Route("api/[controller]")]
