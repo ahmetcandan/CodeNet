129635c69bd874f8df130fd36515896cf877f680
Ahmet CANDAN <candanahm@gmail.com>
2022-01-20 22:53:33 +0300

    Convert map methodu eklendi

--- Net5Api.Core/Converter.cs
+++ Net5Api.Core/Converter.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,7 +10,7 @@ namespace Net5Api.Core
 {
     public static class Converter
     {
-        public static T To<T>(this object value, T defaultValue)
+        public static T Convert<T>(this object value, T defaultValue)
         {
             var result = defaultValue;
             try
@@ -16,24 +18,100 @@ namespace Net5Api.Core
                 if (value == null || value == DBNull.Value) return result;
                 if (typeof(T).IsEnum)
                 {
-                    result = (T)Enum.ToObject(typeof(T), To(value, Convert.ToInt32(defaultValue)));
+                    result = (T)Enum.ToObject(typeof(T), Convert(value, System.Convert.ToInt32(defaultValue)));
                 }
                 else
                 {
-                    result = (T)Convert.ChangeType(value, typeof(T));
+                    result = (T)System.Convert.ChangeType(value, typeof(T));
                 }
             }
             catch (Exception ex)
             {
-                //Tracer.Current.LogException(ex);
+
             }
 
             return result;
         }
 
-        public static T To<T>(this object value)
+        public static T Convert<T>(this object value)
         {
-            return To(value, default(T));
+            return Convert(value, default(T));
+        }
+
+        public static Boolean TryParse<T>(String source, out T value)
+        {
+
+            TypeConverter converter = TypeDescriptor.GetConverter(typeof(T));
+
+            try
+            {
+
+                value = (T)converter.ConvertFromString(source);
+                return true;
+
+            }
+            catch
+            {
+
+                value = default(T);
+                return false;
+
+            }
+
+        }
+
+        public static Boolean TryChangeType<T>(Object source, out T value)
+        {
+
+            try
+            {
+
+                Type type = Nullable.GetUnderlyingType(typeof(T)) ?? typeof(T);
+                value = source.Convert<T>();
+                return true;
+
+            }
+            catch
+            {
+
+                value = default(T);
+                return false;
+
+            }
+
+        }
+
+        public static T Map<T>(this object value) where T : new()
+        {
+            T result = new T();
+            var valueProperties = value.GetType().GetProperties();
+            var convertProperties = typeof(T).GetProperties();
+            MethodInfo convertMethod = null;
+            foreach (var property in (from c in convertProperties
+                                      join v in valueProperties
+                                      on 1 equals 1
+                                      where propertyNameEquals(c.Name, v.Name)
+                                      select new { convertProperty = c, valueProperty = v }))
+            {
+                var propValue = property.valueProperty.GetValue(value);
+                if (property.valueProperty.PropertyType == property.convertProperty.PropertyType)
+                    property.convertProperty.SetValue(result, propValue);
+                else
+                {
+                    if (convertMethod == null)
+                        typeof(Converter).GetMethod("Convert", new Type[] { typeof(object) });
+                    MethodInfo convertMethodExecute = convertMethod.MakeGenericMethod(property.convertProperty.PropertyType);
+                    var resultPropValue = convertMethodExecute.Invoke(null, new object[] { propValue });
+                    property.convertProperty.SetValue(result, resultPropValue);
+                }
+            }
+
+            return result;
+        }
+
+        private static bool propertyNameEquals(string name1, string name2)
+        {
+            return name1.ToLower().Replace("_", "").Equals(name2.Replace("_", "").ToLower());
         }
     }
 }
