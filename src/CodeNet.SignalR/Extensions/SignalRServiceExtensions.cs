a7aeb1447aec23489cc1d5081c3543e6378b663d
Ahmet CANDAN <candanahm@gmail.com>
2024-07-28 12:06:24 +0300

    Add CodeNet.SignalR

---
+++ src/CodeNet.SignalR/Extensions/SignalRServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http.Connections;
+using Microsoft.AspNetCore.SignalR;
+
+namespace CodeNet.SignalR.Extensions;
+
+public static class SignalRServiceExtensions
+{
+    public static HubEndpointConventionBuilder UseSignalR<THub>(this WebApplication app, string pattern)
+        where THub : Hub
+    {
+        return app.MapHub<THub>(pattern);
+    }
+    public static HubEndpointConventionBuilder UseSignalR<THub>(this WebApplication app, string pattern, Action<HttpConnectionDispatcherOptions> configureOptions)
+        where THub : Hub
+    {
+        return app.MapHub<THub>(pattern, configureOptions);
+    }
+}
---
+++ src/CodeNet.SignalR/Hub/NotificationsHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace CodeNet.SignalR;
+
+internal class NotificationsHub : Hub
+{
+    public Task SendNotification(string method, object content, CancellationToken cancellationToken)
+    {
+        return Clients.All.SendAsync(method, content, cancellationToken);
+    }
+}
