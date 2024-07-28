4ce94027b4b0f7cb1a1e99d54511648e7e40e1d1
Ahmet CANDAN <candanahm@gmail.com>
2024-07-29 00:05:06 +0300

    StackExchangeRedis+

--- src/CodeNet.StackExchange.Redis/Services/StackExchangeProducerService.cs
+++ src/CodeNet.StackExchange.Redis/Services/StackExchangeProducerService.cs
@@ -1,19 +1,25 @@
 ﻿using CodeNet.StackExchange.Redis.Settings;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 using StackExchange.Redis;
 
 namespace CodeNet.StackExchange.Redis.Services;
 
 public class StackExchangeProducerService(IOptions<StackExchangeProducerOptions> options)
 {
+    public Task<bool> PublishAsync(object data)
+    {
+        var json = JsonConvert.SerializeObject(data);
+        return PublishAsync(json);
+    }
+
     public async Task<bool> PublishAsync(string message)
 	{
 		try
 		{
             var connection = await ConnectionMultiplexer.ConnectAsync(options.Value.Configuration);
             var subscriber = connection.GetSubscriber();
-            await subscriber.PublishAsync(RedisChannel.Pattern(options.Value.Channel), message, options.Value.CommandFlags);
-            return true;
+            return (await subscriber.PublishAsync(RedisChannel.Pattern(options.Value.Channel), message, options.Value.CommandFlags)) > 0;
         }
         catch
         {
