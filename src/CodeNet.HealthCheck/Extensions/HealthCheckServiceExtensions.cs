807fcbf4f94b49d05c52ba1248e1a6fd00070327
Ahmet CANDAN <candanahm@gmail.com>
2024-07-31 22:22:09 +0300

    HealthChecks

--- src/CodeNet.HealthCheck/Extensions/HealthCheckServiceExtensions.cs
+++ src/CodeNet.HealthCheck/Extensions/HealthCheckServiceExtensions.cs
@@ -14,12 +14,19 @@ public static class HealthCheckServiceExtensions
     /// Add CodeNet Health Check
     /// </summary>
     /// <param name="services"></param>
+    /// <param name="action"></param>
     /// <param name="timeSpan"></param>
     /// <returns></returns>
-    public static IHealthChecksBuilder AddCodeNetHealthCheck(this IServiceCollection services, TimeSpan? timeSpan = null)
+    public static IServiceCollection AddHealthChecks(this IServiceCollection services, Action<IHealthChecksBuilder> action, TimeSpan? timeSpan = null)
     {
         services.AddCodeNetContext();
-        return services.AddHealthChecks().AddCheck<CodeNetHealthCheck>("codenet", HealthStatus.Unhealthy, ["codenet", "identity-context"], timeSpan ?? TimeSpan.FromSeconds(5));
+        HealthCheckServiceCollectionExtensions.AddHealthChecks(services);
+        return services;
+    }
+
+    public static IHealthChecksBuilder AddCodeNetHealthCheck(this IHealthChecksBuilder builder, TimeSpan? timeSpan = null)
+    {
+        return builder.AddCheck<CodeNetHealthCheck>("codenet", HealthStatus.Unhealthy, ["codenet", "identity-context"], timeSpan ?? TimeSpan.FromSeconds(5));
     }
 
     /// <summary>
