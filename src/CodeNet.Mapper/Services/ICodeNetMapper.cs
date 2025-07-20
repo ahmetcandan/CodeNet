72b0cc0382434504d8bb2a374836fac1a48c2cb3
Ahmet CANDAN <candanahm@gmail.com>
2025-07-21 00:41:27 +0300

    fix

--- src/CodeNet.Mapper/Services/ICodeNetMapper.cs
+++ src/CodeNet.Mapper/Services/ICodeNetMapper.cs
@@ -1,10 +1,8 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
-namespace CodeNet.Mapper.Services;
+﻿namespace CodeNet.Mapper.Services;
 
 public interface ICodeNetMapper
 {
-    TDestination? MapTo<TSource, TDestination>([NotNull] TSource source)
+    TDestination? MapTo<TSource, TDestination>(TSource source)
         where TDestination : new()
         where TSource : new();
 }
