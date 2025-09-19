dd865e5eddb5187b746202d18b007b0569154998
Ahmet CANDAN <candanahm@gmail.com>
2025-09-19 20:49:47 +0300

    Transport fix

--- src/CodeNet.MongoDB/Repositories/IMongoDBRepository.cs
+++ src/CodeNet.MongoDB/Repositories/IMongoDBRepository.cs
@@ -1,9 +1,5 @@
-﻿using System.Linq.Expressions;
-
-namespace CodeNet.MongoDB.Repositories;
+﻿namespace CodeNet.MongoDB.Repositories;
 
 public interface IMongoDBRepository<TModel> : IMongoDBSyncRepository<TModel>, IMongoDBAsyncRepository<TModel> where TModel : class
 {
-    TModel Create(TModel model);
-    Task UpdateAsync(Expression<Func<TModel, bool>> filter, IList<KeyValuePair<string, object>> updatedValues, CancellationToken cancellationToken);
 }
