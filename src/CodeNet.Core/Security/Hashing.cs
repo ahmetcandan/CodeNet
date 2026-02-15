630766617e7a1d7537fdfa95fc9b39dfe36378bb
Ahmet CANDAN <candanahm@gmail.com>
2026-02-15 21:15:53 +0300

    Security bugs fix

--- src/CodeNet.Core/Security/Hashing.cs
+++ src/CodeNet.Core/Security/Hashing.cs
@@ -4,17 +4,9 @@ namespace CodeNet.Core.Security
 {
     public static class Hashing
     {
-        public static string MD5(string input) => MD5(Encoding.UTF8.GetBytes(input));
-
-        public static string MD5(byte[] inputBytes) => BitConverter.ToString(System.Security.Cryptography.MD5.HashData(inputBytes)).Replace("-", "");
-
         public static string Shake128(string input) => Shake128(Encoding.UTF8.GetBytes(input));
 
         public static string Shake128(byte[] inputBytes) => BitConverter.ToString(System.Security.Cryptography.Shake128.HashData(inputBytes, inputBytes.Length)).Replace("-", "");
-
-        public static string SHA1(string input) => SHA1(Encoding.UTF8.GetBytes(input));
-
-        public static string SHA1(byte[] inputBytes) => BitConverter.ToString(System.Security.Cryptography.SHA1.HashData(inputBytes)).Replace("-", "");
 
         public static string SHA256(string input) => SHA256(Encoding.UTF8.GetBytes(input));
 
